f12106c270b3f0f1e67a006b2d0c013f3fd338b3
naughtybear971 <iman.fardi97@gmail.com>
2019-09-23 07:53:57 +0800

    'enter'

--- steam/models.py
+++ steam/models.py
@@ -5,9 +5,9 @@ from PIL import Image
 
 class Steam(models.Model):
     gametitle=models.CharField(max_length=120)
-    price=models.CharField(max_length=120)
+    price=models.DecimalField(max_digits=6, decimal_places=2)
     image=models.ImageField()
-    tag=models.ImageField()
+    tag=models.CharField(max_length=120)
     url=models.TextField()
 
 
--- steam/views.py
+++ steam/views.py
@@ -1,6 +1,7 @@
 import shutil
 import ntpath
 import os.path
+from decimal import *
 import os
 from bs4 import BeautifulSoup
 from django.shortcuts import render
@@ -24,43 +25,53 @@ def steam(request):
     topseller = soup.find('div', {'id': 'tab_content_TopSellers'})
     steam = topseller.find_all('a', {'class': 'tab_item'})
 
-    for i in steam:
-        
-            # for steam in soup.find_all('a',{'class':'tab_item'}): # return as a list
+    try:
+        for i in steam:
+            
+                # for steam in soup.find_all('a',{'class':'tab_item'}): # return as a list
 
-            title = i.find('div', {'class': 'tab_item_name'}).text
-            price = i.find('div', {'class': 'discount_final_price'}).text
-            image = i.find('img', {'class': 'tab_item_cap_img'})['src']
-            """
-            tags = i.find_all('span', {'class': 'top_tag'})
+                title = i.find('div', {'class': 'tab_item_name'}).text
+                price = i.find('div', {'class': 'discount_final_price'}).text
+                newprice=(Decimal(price.strip('RM')))
+                #print(newprice)
+                image = i.find('img', {'class': 'tab_item_cap_img'})['src']
+                tags = i.find_all('span', {'class': 'top_tag'})
+                
+                gametags = []
+                for u in tags:
+                    gametags.append(u.string.replace(","," "))
 
-            for u in tags:
-                gametag=u.string
+                #print(gametags)
+                    
+                
+                media_root = '/FYP/django/rise/media'
+                if not image.startswith(("data:image", "javascript")):
+                # local_filename = image.split('/')[-1].split("?")[0]
+                    local_filename = title+'.jpg'
+                    r = session.get(image, stream=True, verify=False)
+                    with open(local_filename, 'wb') as f:
+                        for chunk in r.iter_content(chunk_size=1024):
+                            f.write(chunk)
+
+                    current_image_absolute_path = os.path.abspath(local_filename)
+                    shutil.move(current_image_absolute_path, media_root)
+                
+                
                 new_steam = Steam()
-                new_steam.tag=tags
-
-            """
-            media_root = '/FYP/django/rise/media'
-            if not image.startswith(("data:image", "javascript")):
-               # local_filename = image.split('/')[-1].split("?")[0]
-                local_filename = title+'.jpg'
-                r = session.get(image, stream=True, verify=False)
-                with open(local_filename, 'wb') as f:
-                    for chunk in r.iter_content(chunk_size=1024):
-                        f.write(chunk)
-
-                current_image_absolute_path = os.path.abspath(local_filename)
-                shutil.move(current_image_absolute_path, media_root)
-   
-    
-            new_steam = Steam()
-            new_steam.gametitle= title
-            #new_steam.url= url
-            new_steam.price= price
-            new_steam.image= local_filename
-            new_steam.save()
-    
+                new_steam.gametitle= title
+                new_steam.tag=gametags
+                #new_steam.url= url
+                new_steam.price= newprice
+                new_steam.image= local_filename
+                new_steam.save()
+    except:
+        pass
         
-    return render(request, 'steam/news.html')
+    context ={
+        'steams': Steam.objects.all()
+    }
+    return render(request, 'steam/gamescrape.html',context)
+    
 # Create your views here.
+            
 #steam()

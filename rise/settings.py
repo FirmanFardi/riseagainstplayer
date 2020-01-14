9676ccff48978b58d284f5d7ac25c17aafe2e5e4
naughtybear971 <iman.fardi97@gmail.com>
2020-01-15 02:23:59 +0800

    new

--- rise/settings.py
+++ rise/settings.py
@@ -141,13 +141,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static_in_env"),
-)
-MEDIA_ROOT = os.path.join(BASE_DIR,'media')
-
 MEDIA_URL = '/media/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static_in_env')]
+MEDIA_ROOT = os.path.join(BASE_DIR,'media_root')
+
+
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
